6dc72b50b8914499a7494fab1b4936e929f56957
jessiewuwu <jwu004@gmail.com>
2015-02-17 08:48:54 -0800

    slideToggle for login and register work

--- public/js/application.js
+++ public/js/application.js
@@ -4,6 +4,17 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#login_form').hide(function(){
+    $('.login_section').on('click', function(){
+      $('#login_form').slideToggle('slow');
+    })
+  })
+
+  $('#register_form').hide(function(){
+    $('.register_section').on('click', function(){
+      $('#register_form').slideToggle('slow');
+    })
+  })
 
 
 });
