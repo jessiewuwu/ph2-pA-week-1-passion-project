289586def60a250bfd4c8cc92522ce5320bd4d20
jessiewuwu <jwu004@gmail.com>
2015-02-17 21:05:08 -0800

    parsing method works, now working on seed_dogs

--- testing_json_object.rb
+++ testing_json_object.rb
@@ -2,20 +2,25 @@ require 'open-uri'
 require 'json'
 
 def parse_dogs_json
-  JSON.parse(IO.read('dogsData.json'))
+  @dog_data = JSON.parse(IO.read('dogsData.json'))
 end
 
 
 # create dog records in our database
-def seed_dogs
-  i = 0
-
+def seed_dogs(data)
+  # data = @dog_data["collection1"]
+  data.map_with_index do | dog ,index |
+    dog_name_string = dog["dog_name"]["text"]#["image"]["src"]
+    Dog.create(name: dog_name_string )
+  end
 
 end
-dog_data["collection1"][i]["dog_name"]["text"]
+
+parse_dogs_json
+p seed_dogs(@dog_data["collection1"])
+# [i]["dog_name"]["text"]
 # p "------------------------------------------"
 
 # File.open( 'dogsData.json', "r") do |f|
 #   JSON.load(f)
 # end
-Dog.create(name: first_dog,  )
