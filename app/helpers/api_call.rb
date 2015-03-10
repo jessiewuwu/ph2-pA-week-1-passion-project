5060bcc37ebc03d05a63510d4abacbd2e4e27974
jessiewuwu <jwu004@gmail.com>
2015-03-09 21:06:01 -0700

    add facebook partial

--- app/helpers/api_call.rb
+++ app/helpers/api_call.rb
@@ -9,6 +9,7 @@ require 'httparty'
 # end
 
 # def get_data
+
 #   response = HTTParty.get('https://www.googleapis.com/customsearch/v1?cx=016871532285775593244%3Ahitwjirk3ac&q=dog%20transparent%20gif&searchType=image&num=10&start=11&key=AIzaSyAy1FwWb_llGer7CJZk65r63ZidHFqdJ9Q')
 #   items = response["items"]
 #   items.map{|item| item["link"] }
